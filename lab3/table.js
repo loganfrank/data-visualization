e38fea9d71b2f4ef060c47f24b9ccd0c45c7e67b
Logan Frank <frank.580@osu.edu>
2020-10-21 18:04:38 -0400

    some stuff

--- lab3/table.js
+++ lab3/table.js
@@ -1,3 +1,25 @@
 function table(state_to_cases, state_month_data) {
+    var table = d3.select('#table_svg').append('table');
+    var header = table.append('thread').append('tr');
+    header.selectAll('th')
+        .data(['State', 'Total Cases'])
+        .enter()
+        .append('th')
+        .text(function(d) { return d; });
+}
+
+function update_table() {
+
+}
+
+function update_map() {
+
+}
+
+function update_bar() {
+
+}
+
+function update_pack() {
 
 }
