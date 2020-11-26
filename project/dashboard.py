d2eebf742a8aebc7737e23da259153800cc833e9
Logan Frank <frank.580@osu.edu>
2020-11-26 01:18:01 -0500

    started project

---
+++ project/dashboard.py
@@ -0,0 +1,10 @@
+import plotly
+import plotly.express as px
+import plotly.graph_objects as go 
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+if __name__ == '__main__':
+    print('Hello World')
